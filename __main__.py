741b3a33ca28df08bca706c30f07fa8cea53458e
Wilder Lizama Garcia <wilderlizama@gmail.com>
2019-07-25 18:21:28 -0500

    Notificación Windows 10

--- __main__.py
+++ __main__.py
@@ -1,5 +1,7 @@
 from OPMWebComic import OPMWebComic, OPMWebComicBDActions
 import datetime
+from win10toast import ToastNotifier
+
 from pprint import pprint
 
 
@@ -14,6 +16,24 @@ def get_real_number(number_str):
     real_num = int(number_str) if number_str_dec == 0 else float(number_str)
 
     return real_num
+
+
+def notificar_w10toast(arr_capitulos):
+
+    arr_notf_caps = [str(scap) for scap in arr_capitulos]
+    str_caps = ",".join(arr_notf_caps)
+
+    str_body_notf = "Salió un nuevo capítulo de OPM Webcomic 😱 cap: %s" % str_caps
+    if len(arr_notf_caps) > 1:
+        str_body_notf = "Han salido nuevos capítulos de OPM Webcomic 😱 caps: %s" % str_caps
+
+    toaster = ToastNotifier()
+    toaster.show_toast(
+        "Notificación OPM WC capítulos!",
+        str_body_notf,
+        icon_path=None,
+        threaded=True,
+        duration=30)
 
 
 def main():
@@ -40,13 +60,11 @@ def main():
                 now)
 
             # agregar a array para envio de notificación
-            arr_nuevos_capitulos.append({
-                "cap_numero" : cap_numero,
-                "nombre": content_string,
-                "full_url" : "%s%s" % (opmwc.URL, content_tag_href)
-            })
+            arr_nuevos_capitulos.append(cap_numero)
 
-    print(arr_nuevos_capitulos)
+    if len(arr_nuevos_capitulos) > 0:
+        notificar_w10toast(arr_nuevos_capitulos)
+
 
 if __name__ == "__main__":
     main()
