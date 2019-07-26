03a493c042efb9d6c77f461359ef60b8492ae9b9
Wilder Lizama Garcia <wilderlizama@gmail.com>
2019-07-26 14:41:40 -0500

    Eviar correo Notificacion

--- CommonActions.py
+++ CommonActions.py
@@ -27,40 +27,41 @@ def get_real_number(number_str):
     return real_num
 
 
-def notificar_w10toast(arr_capitulos):
-
-    arr_notf_caps = [str(scap) for scap in arr_capitulos]
-    str_caps = ",".join(arr_notf_caps)
-
-    str_body_notf = "Salió un nuevo capítulo de OPM Webcomic 😱 cap: %s" % str_caps
-    if len(arr_notf_caps) > 1:
-        str_body_notf = "Han salido nuevos capítulos de OPM Webcomic 😱 caps: %s" % str_caps
+def notificar_w10toast(title, body):
 
     toaster = ToastNotifier()
     toaster.show_toast(
-        "Notificación OPM WC capítulos!",
-        str_body_notf,
+        title,
+        body,
         icon_path=None,
         threaded=True,
         duration=30)
 
 
 def sed_mail(asunto, email_emisor, email_emisor_pwd, email_receptor, body):
-    SUBJECT = asunto
-    FROM_ADDR = email_emisor
-    FROM_ADDR_PASS = email_emisor_pwd
-    TO_ADDR = email_receptor
+    is_sended = True
+    try:
+        SUBJECT = asunto
+        FROM_ADDR = email_emisor
+        FROM_ADDR_PASS = email_emisor_pwd
+        TO_ADDR = email_receptor
 
-    msg = email.message.Message()
-    msg["Subject"] = SUBJECT
-    msg["From"] = FROM_ADDR
-    msg["To"] = TO_ADDR
-    msg.add_header('Content-Type','text/html')
-    msg.set_payload(body)
+        msg = email.message.Message()
+        msg["Subject"] = SUBJECT
+        msg["From"] = FROM_ADDR
+        msg["To"] = TO_ADDR
+        msg.add_header('Content-Type','text/html')
+        msg.set_payload(body)
 
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)  # mail server ip
-    server.login(FROM_ADDR, FROM_ADDR_PASS)
-    server.sendmail(msg["from"],msg["To"].split(";"), msg.as_string())
-    server.set_debuglevel(1)
-    server.quit()
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)  # mail server ip
+        server.login(FROM_ADDR, FROM_ADDR_PASS)
+        server.sendmail(msg["from"],msg["To"].split(";"), msg.as_string().encode('utf-8').strip())
+        server.set_debuglevel(1)
+        server.quit()
+    except Exception as ex:
+        is_sended = False
+        print(ex)
+        raise ex
+
+    return is_sended
 
--- __main__.py
+++ __main__.py
@@ -7,12 +7,12 @@ from CommonActions import (
     getConfigValue)
 
 # from pprint import pprint
+NOW = dtnow()
 
 def main():
     opmwc = OPMWebComic()
     opmwc_bd_act = OPMWebComicBDActions()
     webc_lista_capitulos = opmwc.get_lista_capitulos()
-    now = dtnow()
     arr_nuevos_capitulos = []
 
     """ Buscar si capítulo no existe en BD
@@ -29,20 +29,38 @@ def main():
                 cap_numero,
                 content_string,
                 "Capítulo ubicado en %s" % content_tag_href,
-                now)
+                NOW)
 
             # agregar a array para envio de notificación
             arr_nuevos_capitulos.append(cap_numero)
 
     if len(arr_nuevos_capitulos) > 0:
-        notificar_w10toast(arr_nuevos_capitulos)
+        arr_notf_caps = [str(scap) for scap in arr_nuevos_capitulos]
+        str_caps = ",".join(arr_notf_caps)
+
+        str_body_notf = "Salió un nuevo capítulo de OPM Webcomic 😱 cap: %s" % str_caps
+        if len(arr_notf_caps) > 1:
+            str_body_notf = "Han salido nuevos capítulos de OPM Webcomic 😱 caps: %s" % str_caps
+
+        asunto = "Notificación OPM WC, nuevos capítulos"
+        body_mail = """Ha salido un nuevo capitulo de OPM WC
+        <br>
+        Dirección de enlace: %s
+        """ % opmwc.URL
+        for nuevo_capitulo in arr_nuevos_capitulos:
+            body_mail = "%s<br>- Capítulo nro. %s<br>" % (body_mail, nuevo_capitulo)
+
+        is_sended = sed_mail(
+            "%s  😱 %s" % (asunto, NOW),
+            getConfigValue("MAIL", "EMAIL_EMISOR"),
+            getConfigValue("MAIL", "EMAIL_EMISOR_PWD"),
+            getConfigValue("MAIL", "EMAIL_RECEPTOR"),
+            body_mail)
+
+        if is_sended:
+            notificar_w10toast(asunto, str_body_notf)
+            
 
 
 if __name__ == "__main__":
-    # main()
-    sed_mail(
-        "asunto sin importacia OPM WC 01",
-        getConfigValue("MAIL", "EMAIL_EMISOR"),
-        getConfigValue("MAIL", "EMAIL_EMISOR_PWD"),
-        getConfigValue("MAIL", "EMAIL_RECEPTOR"),
-        "Cuerpo de OPM WC !!")
+    main()
